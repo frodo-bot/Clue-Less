fcf93fefb33a0d605a7c0bf5684b537c9df50a3a
unknown <lzetl1@umbc.edu>
2019-10-17 15:39:45 -0400

    Created signup page

--- clueless/views.py
+++ clueless/views.py
@@ -2,6 +2,14 @@
 from django.shortcuts import render
 from django.utils.safestring import mark_safe
 import json
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
 
 def index(request):
     return render(request, 'index.html', {})
+
+class signup(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'signup.html'
