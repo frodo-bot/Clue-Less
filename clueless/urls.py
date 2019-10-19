1f0283850bc6a2077a1ea9ecac98cc5619712935
Arvin Siva <arvinsiva@Arvins-MacBook-Pro.local>
2019-10-19 19:21:35 -0400

    Setup Server Polling
    This is a test of how we might implement polling. I have some dummy data returned along with an incrementing num on the /game endpoint.

--- clueless/urls.py
+++ clueless/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('signup/', views.signup.as_view(), name='signup'),
+    path('game/', views.getGameState, name='game'),
 ]
--- clueless/views.py
+++ clueless/views.py
@@ -5,6 +5,9 @@ import json
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
+from django.http import JsonResponse
+
+num = 0
 
 def index(request):
     return render(request, 'index.html', {})
@@ -13,3 +16,12 @@ class signup(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+def getGameState(request):
+    global num
+    num += 1
+    data = {
+        "messages" : ["Example Message 1", "Example Message 2"],
+        "num" : num
+    }
+    return JsonResponse(data)
