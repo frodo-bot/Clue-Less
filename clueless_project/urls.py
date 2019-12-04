f7f7ab8e79aacc8fdd2bc6737168ed779c65f0c1
unknown <lzetl1@umbc.edu>
2019-12-04 17:08:56 -0500

    did some styling of entry pages, added logo

--- clueless_project/urls.py
+++ clueless_project/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from django.contrib.auth import views
+from clueless_project.forms import LoginForm
 
 urlpatterns = [
     path('', include('clueless.urls')),
-    path('', include('django.contrib.auth.urls')),
+    path('', include('django.contrib.auth.urls'), {'authentication_form': LoginForm}),
+    #path('login/', ),
     path('admin/', admin.site.urls),
 ]
